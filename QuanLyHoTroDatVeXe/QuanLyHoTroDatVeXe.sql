add7ba08a52cd2d4e31010155d795fd287e7374c
hoangyen612 <thienanh156@gmail.com>
2019-10-23 19:56:48 +0700

    fix fQuanLyXe

--- QuanLyHoTroDatVeXe/QuanLyHoTroDatVeXe.sql
+++ QuanLyHoTroDatVeXe/QuanLyHoTroDatVeXe.sql
@@ -10,8 +10,7 @@ CREATE TABLE Xe
 	bienSo VARCHAR(10) PRIMARY KEY,
 	taiXe NVARCHAR(50) NOT NULL,
 	sdtTaiXe INT,
-	tenXe NVARCHAR(50) NOT NULL,
-	trangThai INT
+	tenXe NVARCHAR(50) NOT NULL
 )
 GO
 --Chuyến đi
@@ -64,11 +63,11 @@ GO
 ---STORE  PROC
 --Thêm xe
 CREATE PROC themXe 
-(@bienSo VARCHAR(10),@taiXe NVARCHAR(50), @sdtTaiXe int, @tenXe NVARCHAR(50), @trangThai INT)
+(@bienSo VARCHAR(10),@taiXe NVARCHAR(50), @sdtTaiXe int, @tenXe NVARCHAR(50))
 AS
 BEGIN
-	INSERT dbo.Xe ( bienSo, taiXe, sdtTaiXe, tenXe , trangThai)
-	VALUES  ( @bienSo, @taiXe, @sdtTaiXe, @tenXe, @trangThai)
+	INSERT dbo.Xe ( bienSo, taiXe, sdtTaiXe, tenXe)
+	VALUES  ( @bienSo, @taiXe, @sdtTaiXe, @tenXe)
 END
 GO
 --Thêm chuyến đi
@@ -93,51 +92,44 @@ CREATE PROC themVeXe (@soDienThoai INT, @maCD INT, @maGhe VARCHAR(3), @thoiGianD
 AS
 BEGIN
 	INSERT dbo.VeXe(soDienThoai, maCD, maGhe, thoiGianDat )
-	VALUES  (@maCD, @maGhe, @maGhe, @thoiGianDat)
+	VALUES  (@soDienThoai, @maCD, @maGhe, @thoiGianDat)
 END
 GO
 ---Dữ liệu
 --Xe
-themXe '59F-61792', N'Lê Quốc Hoàng', 328893485, N'KIA', 0
+themXe '59F-61792', N'Lê Quốc Hoàng', 328893485, N'KIA'
 GO
-themXe '39F-81792', N'Trần Nam Thương', 123457890, N'Toyota', 1
+themXe '39F-81792', N'Trần Nam Thương', 123457890, N'Toyota'
 GO
-themXe '62M-53012', N'Nguyễn Quốc Vương', 102458079, N'Limouse', 1
+themXe '62M-53012', N'Nguyễn Quốc Vương', 102458079, N'Limouse'
 GO
-themXe '48K-10054', N'Vũ Hoàng Nam', 112589774, N'Limouse Phương Trang', 1
+themXe '48K-10054', N'Vũ Hoàng Nam', 112589774, N'Limouse Phương Trang'
 GO
-themXe '66T-10054', N'Cao Bá Đạt', 857493050, N'KIA', 1
+themXe '66T-10054', N'Cao Bá Đạt', 857493050, N'KIA'
 GO
-themXe '62L-10054', N'Trần Văn Anh', 205793240, N'Limouse', 1
+themXe '62L-10054', N'Trần Văn Anh', 205793240, N'Limouse'
 GO
-themXe '74L-98754', N'nguyễn Kim Thiện', 971528030, N'Toyota', 1
+themXe '74L-98754', N'nguyễn Kim Thiện', 971528030, N'Toyota'
 GO
 --Chuyến đi
-themChuyenDi '5h00', '12-06-2019' , N'Đồng Nai', 'LongAn', 125000, '39F-81792' 
+themChuyenDi '5h00', '12-06-2019' , N'Đồng Nai', 'LongAn', 180000, '39F-81792' 
 GO
 themChuyenDi '6h00', '11-5-2019' , N'tp.Hồ Chí Minh', 'LongAn', 125000, '62M-53012' 
 GO
 themChuyenDi '7h00', '11-3-2019' , N'tp.Hồ Chí Minh', N'Đồng Tháp', 125000, '48K-10054' 
 GO
-themChuyenDi '8h00', '11-7-2013' , N'tp.Hồ Chí Minh', N'Bảo Lộc', 125000, '66T-10054' 
+themChuyenDi '8h00', '11-7-2013' , N'tp.Hồ Chí Minh', N'Bảo Lộc', 250000, '66T-10054' 
 GO
-themChuyenDi '9h00', '11-7-2013' , N'Đồng Nai', N'Bảo Lộc', 125000, '62L-10054' 
+themChuyenDi '9h00', '11-7-2013' , N'Đồng Nai', N'Bảo Lộc', 100000, '62L-10054' 
 GO
-themChuyenDi '10h00', '11-7-2013' , N'Đồng Nai', N'Đồng Tháp', 125000, '74L-98754' 
+themChuyenDi '10h00', '11-7-2013' , N'Đồng Nai', N'Đồng Tháp', 200000, '74L-98754' 
 GO
 --Tài khoản
-INSERT INTO TaiKhoan (tenDangNhap, matKhau, loaiTaiKhoan) VALUES ( '0', '0', 0),( '1', '1', 1)
+INSERT INTO TaiKhoan (tenDangNhap, matKhau, loaiTaiKhoan) VALUES ( 'yen', '123', 0),( 'tu2909', '123', 1)
 GO
 --Khách hàng
-themKH 918236031, 251123456, N'Hoàng Yến', N'Nữ', 'tp.HCM', 'yen.th@gmail.com', '1'
+themKH 918236031, 251123456, N'Hoàng Yến', N'Nữ', 'tp.HCM', 'yen.th@gmail.com', 'yen'
 GO
-themKH 912839740, 251123456, N'Thanh Tú', 'Nam', 'tp.HCM', 'tu.nt@gmail.com', '0'
+themKH 912839740, 251123456, N'Thanh Tú', 'Nam', 'tp.HCM', 'tu.nt@gmail.com', 'tu2909'
 GO
 --Vé xe
-
-
-
-
-
-
-
